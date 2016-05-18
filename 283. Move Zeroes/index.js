cb8d3559decc5881a6e2da0edf63b1be340b343d
zj <mail@yangshaobo.cn>
2016-05-18 11:50:07 +0800

    debug 283

--- 283. Move Zeroes/index.js
+++ 283. Move Zeroes/index.js
@@ -3,17 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    var time = 0;
-    for(var i = 0;i < nums.length;i++){
-        if(nums[i] == 0){
-            nums.splice(i,1);
-            var zero = 0;
-            nums.push(zero);
-            i--;
-            time++;
-            if(time == nums.length) break;
-        }else{
-            time = 0;
+    var zeroCount = 0;
+    for(var i = 0 ; i< nums.length; i++) {
+        if (nums[i] === 0) {
+            zeroCount++;
+        } else if (zeroCount > 0 ){
+            nums[i-zeroCount] = nums[i];
+            nums[i] = 0;
         }
     }
 };
