408b3e390794f18ec4ea18e4ecdc83295bffb5a0
zj <mail@yangshaobo.cn>
2016-05-18 19:10:35 +0800

    100. Same Tree

---
+++ 100. Same Tree/index.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if(p === null && q === null) return true;
+    if(p === null || q === null) return false;
+    return (p.val === q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+};
