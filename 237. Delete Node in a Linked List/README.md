f8b7499bfc0b380cb6e35b0fd36e784b0983c986
zj <mail@yangshaobo.cn>
2016-05-18 15:08:39 +0800

    237. Delete Node in a Linked List

---
+++ 237. Delete Node in a Linked List/README.md
@@ -0,0 +1,1 @@
+# 237. Delete Node in a Linked List
---
+++ 237. Delete Node in a Linked List/index.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    var nextNode = node.next;
+    node.val = nextNode.val;
+    node.next = nextNode.next;
+};
