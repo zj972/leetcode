951db496e026d7aaf38031515398cd36d5cd133e
zj <mail@yangshaobo.cn>
2016-05-19 13:39:30 +0800

    349. Intersection of Two Arrays

---
+++ 349. Intersection of Two Arrays/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    var sam = [];
+    var b = 0;
+    for(var i = 0;i < nums2.length;i++){
+        for(var j = 0;j < nums1.length;j++){
+            if(nums2[i] === nums1[j]){
+                b = 1;
+            }
+        }
+        if(b === 1){
+            for(var x = 0,c = 0;x < sam.length;x++){
+                if(nums2[i] === sam[x]){
+                    c = 1;  
+                }
+            }
+            if(!c){
+                sam.push(nums2[i]);
+            }
+            b = 0;
+        }
+    }
+    return sam;
+};
