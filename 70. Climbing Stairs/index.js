b6957a2b406ca3527a6c638df6e58081d5607b10
zj <mail@yangshaobo.cn>
2016-05-20 23:10:31 +0800

    70. Climbing Stairs

---
+++ 70. Climbing Stairs/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n <= 2) return n;
+    var step = 3, s1 = 1, s2 = 2, tmp;
+    while (step <= n) {
+        tmp = s1 + s2;
+        s1 = s2;
+        s2 = tmp;
+        ++step;
+    }
+    return s2;
+};
