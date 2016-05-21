f02e8304513e0bc1bfcb9cb5b08498e2cebbcbec
zj <mail@yangshaobo.cn>
2016-05-21 15:10:18 +0800

    121. Best Time to Buy and Sell Stock

---
+++ code/121. Best Time to Buy and Sell Stock/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var n = prices.length;
+    var min = Number.MAX_VALUE;
+    var money = 0;
+    for(var i = 0; i < n; i++){
+        if(prices[i] < min){
+            min = prices[i];
+        }else{
+            if((prices[i] - min) > money){
+                money = prices[i] - min;
+            }
+        }
+    }
+    return money;
+};
