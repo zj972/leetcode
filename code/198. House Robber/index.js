6e10a1f537f18ee869c67f42e9c3eafc0bba566e
zj <mail@yangshaobo.cn>
2016-05-21 22:01:04 +0800

    198. House Robber

---
+++ code/198. House Robber/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (nums === null || nums.length === 0) {
+        return 0;
+    }
+    if (nums.length > 2) {
+        nums[2] += nums[0];
+    }
+    for (var i = 3; i < nums.length; i++) {
+        nums[i] += Math.max(nums[i - 2], nums[i - 3]);
+    }
+    if (nums.length == 1) {
+        return nums[0];
+    }else if (nums.length == 2) {
+        return Math.max(nums[0], nums[1]);
+    }else {
+        return Math.max(nums[i - 1], nums[i - 2]);
+    }
+};
