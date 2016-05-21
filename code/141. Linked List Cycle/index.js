86d2d7678570c0af9d1bfcde65f46e93c0871815
zj <mail@yangshaobo.cn>
2016-05-21 14:24:40 +0800

    141. Linked List Cycle

---
+++ code/141. Linked List Cycle/index.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head || !head.next) return false;
+    var n = head, m = head;
+    while(1){
+        if(n && n.next){
+            n = n.next;
+        }else{
+            return false;
+        }
+        if(m && m.next && m.next.next){
+            m = m.next.next;
+        }else{
+            return false;
+        }
+        if(n === m) return true;
+    }
+    return false;
+};
