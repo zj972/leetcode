16cdcbc8bae3d0fadbb89e10a6cfa000e818d8ab
zj <mail@yangshaobo.cn>
2016-05-21 21:23:22 +0800

    345. Reverse Vowels of a String

---
+++ code/345. Reverse Vowels of a String/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    s = s.split("");
+    var x = 0;
+    var y = s.length;
+    while(1){
+        if(x >= y) return s.join("");
+        if(s[x] === "a" || s[x] === "e" || s[x] === "i" || s[x] === "o" || s[x] === "u" || s[x] === "A" || s[x] === "E" || s[x] === "I" || s[x] === "O" || s[x] === "U"){
+            if(s[y] === "a" || s[y] === "e" || s[y] === "i" || s[y] === "o" || s[y] === "u" || s[y] === "A" || s[y] === "E" || s[y] === "I" || s[y] === "O" || s[y] === "U"){
+                var temp = s[x];
+                s[x] = s[y];
+                s[y] = temp;
+                x++;
+                y--;
+            }else{
+                y--;
+            }
+        }else{
+            x++;
+        }
+    }
+};
