b232474a0c203dcfe62b64cc72ed98e72644ac64
zj <mail@yangshaobo.cn>
2016-05-22 11:55:30 +0800

    27. Remove Element

---
+++ code/027. Remove Element/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var valList = 0;
+    for(var i = 0, n = nums.length; i < n; i++){
+        if(nums[i] === val){
+            nums.splice(i,1);
+            i--;
+        }else{
+            valList++;
+        }
+    }
+    return valList;
+};
