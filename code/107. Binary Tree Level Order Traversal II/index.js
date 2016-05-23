9d235e63feb14c957b8d76bbca631b9bff24b2c1
zj <mail@yangshaobo.cn>
2016-05-23 22:27:41 +0800

    107. Binary Tree Level Order Traversal II

---
+++ code/107. Binary Tree Level Order Traversal II/index.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    var num = 0;
+    var rootArray = [];
+    function level(root){
+        if(root){
+            if(rootArray[num] === undefined) rootArray[num] = [];
+            rootArray[num].push(root.val);
+            num++;
+            level(root.left);
+            level(root.right);
+            num--;
+        }
+    }
+    level(root);
+    rootArray = rootArray.reverse();
+    return rootArray;
+};
