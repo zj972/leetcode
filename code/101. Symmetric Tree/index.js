a2ffdf0c45ea639dd85b1da484412ba2c2a0b8a0
zj <mail@yangshaobo.cn>
2016-05-23 23:31:53 +0800

    101. Symmetric Tree && 102 debug

---
+++ code/101. Symmetric Tree/index.js
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(!root) return true;
+    var num = 1;
+    var rootArray = [root.val];
+    function level(root){
+        if(root){
+            if(rootArray[num] === undefined) rootArray[num] = [];
+            if(root.left){
+                rootArray[num].push(root.left.val);
+            }else{
+                rootArray[num].push(null);
+            }
+            if(root.right){
+                rootArray[num].push(root.right.val);
+            }else{
+                rootArray[num].push(null);
+            }
+            num++;
+            level(root.left);
+            level(root.right);
+            num--;
+        }
+    }
+    level(root);
+    rootArray.length--;
+    for(var i = 0, n = rootArray.length; i < n; i++){
+        var x = 0;
+        var y = rootArray[i].length-1;
+        while(x <= y){
+            if(rootArray[i][x] !== rootArray[i][y]){
+                return false;
+            }else{
+                x++;
+                y--;
+            }
+        }
+    }
+    return true;
+};
+// var isSymmetric = function(root) {
+//     function isSymmetricHelp(left,right){
+//         if(left === null || right === null)
+//             return left === right;
+//         if(left.val !== right.val)
+//             return false;
+//         return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
+//     }
+//     return root === null || isSymmetricHelp(root.left, root.right);
+// };
--- code/102. Binary Tree Level Order Traversal/index.js
+++ code/102. Binary Tree Level Order Traversal/index.js
@@ -10,5 +10,18 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    
+    var num = 0;
+    var rootArray = [];
+    function level(root){
+        if(root){
+            if(rootArray[num] === undefined) rootArray[num] = [];
+            rootArray[num].push(root.val);
+            num++;
+            level(root.left);
+            level(root.right);
+            num--;
+        }
+    }
+    level(root);
+    return rootArray;
 };
