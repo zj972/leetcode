616c8a5b44a4249949f5d914b579e7b10184e49c
zj <mail@yangshaobo.cn>
2016-05-27 16:51:42 +0800

    232. Implement Queue using Stacks

---
+++ code/232. Implement Queue using Stacks/index.js
@@ -0,0 +1,35 @@
+/**
+ * @constructor
+ */
+var Queue = function() {
+    this.queue = [];
+};
+
+/**
+ * @param {number} x
+ * @returns {void}
+ */
+Queue.prototype.push = function(x) {
+    this.queue.push(x);
+};
+
+/**
+ * @returns {void}
+ */
+Queue.prototype.pop = function() {
+    this.queue.shift();
+};
+
+/**
+ * @returns {number}
+ */
+Queue.prototype.peek = function() {
+    return this.queue[0];
+};
+
+/**
+ * @returns {boolean}
+ */
+Queue.prototype.empty = function() {
+    return this.queue.length === 0;
+};
