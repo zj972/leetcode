ac6af9d10942e365673f4a1d6ba19f3daf714ee1
zj <mail@yangshaobo.cn>
2016-05-28 13:12:19 +0800

    290. Word Pattern

---
+++ code/290. Word Pattern/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, str) {
+    var list1 = {};
+    str = str.split(" ");
+    if(pattern.length !== str.length) return false;
+    for(var i = 0, n = pattern.length; i < n; i++){
+        if(list1[pattern[i]]){
+            if(list1[pattern[i]] !== str[i]) return false;
+        }else{
+            list1[pattern[i]] = str[i];
+        }
+    }
+    var list2 = {};
+    for(var j = 0; j < n; j++){
+        if(list2[str[j]]){
+            if(list2[str[j]] !== pattern[j]) return false;
+        }else{
+            list2[str[j]] = pattern[j];
+        }
+    }
+    return true;
+};
