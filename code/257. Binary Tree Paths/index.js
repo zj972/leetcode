0f9243d1f1784841f8c1ff48e84807331e141e8c
zj <mail@yangshaobo.cn>
2016-05-28 22:32:03 +0800

    257. Binary Tree Paths

---
+++ code/257. Binary Tree Paths/index.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    function searchBT(root, path, answer) {
+        if (root.left === null && root.right === null) answer.push(path + root.val);
+        if (root.left !== null) searchBT(root.left, path + root.val + "->", answer);
+        if (root.right !== null) searchBT(root.right, path + root.val + "->", answer);
+    }
+    var answer = [];
+    if (root !== null) searchBT(root, "", answer);
+    return answer;
+};
