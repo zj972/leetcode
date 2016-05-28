54a3d34e048d41e39733fe6a913f67efa94cb0dc
zj <mail@yangshaobo.cn>
2016-05-28 12:49:03 +0800

    20. Valid Parentheses

---
+++ code/020. Valid Parentheses/index.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    var list = [];
+    for(var i = 0, n = s.length; i < n; i++){
+        switch(s[i]){
+            case "(" :
+                list.push(s[i])
+                break;
+            case ")" :
+                if(list[list.length -1] === "("){
+                    list.length--;
+                }else{
+                    return false;
+                }
+                break;
+            case "{" :
+                list.push(s[i])
+                break;
+            case "}" :
+                if(list[list.length -1] === "{"){
+                    list.length--;
+                }else{
+                    return false;
+                }
+                break;
+            case "[" :
+                list.push(s[i])
+                break;
+            case "]" :
+                if(list[list.length -1] === "["){
+                    list.length--;
+                }else{
+                    return false;
+                }
+                break;
+            default  : break;
+        }
+    }
+    return !list.length;
+};
