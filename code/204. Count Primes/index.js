d7a77369291ee1c6406e65782e4b7857a5a51822
zj <mail@yangshaobo.cn>
2016-05-29 14:01:12 +0800

    204 优化

--- code/204. Count Primes/index.js
+++ code/204. Count Primes/index.js
@@ -2,17 +2,47 @@
  * @param {number} n
  * @return {number}
  */
+ /**
+ * @param {number} n
+ * @return {number}
+ */
+ //runtime: 63.89% 376ms
 var countPrimes = function(n) {
-    if(!n) return n;
-    if(n === 1) return 0;
-    var num = [];
-    var m = 0;
+    if(n <= 2){
+        return 0;
+    }
+    var mem = [];
     for(var i = 2; i < n; i++){
-        if(!num[i]) m++;
-        for(var j = i; j < n;){
-            num[j] = 1;
-            j += i;
+        mem[i] = true;
+    }
+    sq = parseInt(Math.sqrt(n - 1));
+    for(i = 2; i <= sq; i++){
+        if(mem[i]){
+            for(var j = i + i; j < mem.length; j += i){
+                mem[j] = false;
+            }
         }
     }
-    return m;
+    var count = 0;
+    for(i = 2; i < mem.length; i++){
+        if(mem[i]){
+            count++;    
+        }
+    }
+    return count;
 };
+//runtime 31% 660ms
+// var countPrimes = function(n) {
+//     if(!n) return n;
+//     if(n === 1) return 0;
+//     var num = [];
+//     var m = 0;
+//     for(var i = 2; i < n; i++){
+//         if(!num[i]) m++;
+//         for(var j = i; j < n;){
+//             num[j] = 1;
+//             j += i;
+//         }
+//     }
+//     return m;
+// };
