261b3e2145f6b85dccf3c91e6ff330738d826376
zj <mail@yangshaobo.cn>
2016-05-29 10:57:49 +0800

    14. Longest Common Prefix

---
+++ code/014. Longest Common Prefix/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(!strs.length) return "";
+    var str = strs[0];
+    for(var i = 1; i < strs.length; i++){
+        for(var j = 0,n = Math.max(strs[i].length,str.length); j < n; j++){
+            if(!strs[i][j]){
+                str = str.substr(0,j);
+            }else if(!str[j]){
+                continue;
+            }else if(str[j] !== strs[i][j]){
+                str = str.substr(0,j);
+            }
+        }
+    }
+    return str;
+};
