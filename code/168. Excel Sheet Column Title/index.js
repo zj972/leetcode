56960f21f22f204f19007c15b6ce7877e9a4c28b
zj <mail@yangshaobo.cn>
2016-05-30 12:34:58 +0800

    168 优化

--- code/168. Excel Sheet Column Title/index.js
+++ code/168. Excel Sheet Column Title/index.js
@@ -3,17 +3,22 @@
  * @return {string}
  */
 var convertToTitle = function(n) {
-    var list = "";
-    var go = 0;
-    while(n > 26){
-        go = n % 26;
-        if(go === 0){
-            go = 26;
-            n = n/26 - 1;
-        }
-        else n = (n - go)/26;
-        list = String.fromCharCode(go+64) + list;
-    }
-    list = String.fromCharCode(n+64) + list;
-    return list;
+    if (n === 0) return "";
+    return convertToTitle(parseInt((n - 1) / 26)) + String.fromCharCode((n - 1) % 26 + 65);
 };
+//old
+// var convertToTitle = function(n) {
+//     var list = "";
+//     var go = 0;
+//     while(n > 26){
+//         go = n % 26;
+//         if(go === 0){
+//             go = 26;
+//             n = n/26 - 1;
+//         }
+//         else n = (n - go)/26;
+//         list = String.fromCharCode(go+64) + list;
+//     }
+//     list = String.fromCharCode(n+64) + list;
+//     return list;
+// };
