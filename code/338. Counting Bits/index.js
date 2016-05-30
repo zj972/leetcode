5470774213cc2bb9219ea4da298816db9ec192c6
zj <mail@yangshaobo.cn>
2016-05-30 18:45:17 +0800

    338. Counting Bits

---
+++ code/338. Counting Bits/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+//runtime 86.11% 284ms
+var countBits = function(num) {
+    var res = [0];
+    for(var i=1;i<=num;i++){
+        res[i] = res[i >> 1] + (i & 1);
+    }
+    return res;
+};
+//runtime 61.81% 300ms
+// var countBits = function(num) {
+//     var res = [];
+//     var cnt = 1;
+//     var flag = 1;
+//     res.push(0);
+//     while(cnt <= num){
+//         var start = cnt;
+//         for (var tmp = flag; tmp > 0 && cnt <= num; --tmp){
+//             res.push(1 + res[start-tmp]);
+//             cnt++;
+//         }
+//         flag *= 2;
+//     }
+//     return res;
+// };
