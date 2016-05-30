f73cb5fe21aa8649b30e7c508917684c2d7fa269
zj <mail@yangshaobo.cn>
2016-05-30 13:16:44 +0800

    165. Compare Version Numbers

---
+++ code/165. Compare Version Numbers/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    version1 = version1.split(".");
+    version2 = version2.split(".");
+    for(var i = 0, n = Math.max(version1.length,version2.length); i < n; i++){
+        if(!version1[i] && ! version2[i]) return 0;
+        else if(!version1[i] && parseInt(version2[i])) return -1;
+        else if(parseInt(version1[i]) && !version2[i]) return 1;
+        else if(parseInt(version1[i]) > parseInt(version2[i])) return 1;
+        else if(parseInt(version1[i]) < parseInt(version2[i])) return -1;
+    }
+    return 0;
+};
