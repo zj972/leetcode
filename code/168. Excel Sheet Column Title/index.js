84d9e3843e3d3ab7880bd270694968f8a859f0ad
zj <mail@yangshaobo.cn>
2016-05-30 12:31:24 +0800

    168. Excel Sheet Column Title

---
+++ code/168. Excel Sheet Column Title/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function(n) {
+    var list = "";
+    var go = 0;
+    while(n > 26){
+        go = n % 26;
+        if(go === 0){
+            go = 26;
+            n = n/26 - 1;
+        }
+        else n = (n - go)/26;
+        list = String.fromCharCode(go+64) + list;
+    }
+    list = String.fromCharCode(n+64) + list;
+    return list;
+};
