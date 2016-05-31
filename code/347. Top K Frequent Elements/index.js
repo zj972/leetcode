e9d74daeb220e2aefb61aca937b9825ef3598c1b
zj <mail@yangshaobo.cn>
2016-05-31 11:16:34 +0800

    347. Top K Frequent Elements

---
+++ code/347. Top K Frequent Elements/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    var map = {};
+    for(var i = 0, n = nums.length; i < n; i++){
+        if(map[nums[i]] === undefined){
+            map[nums[i]] = 1;
+        }else{
+            map[nums[i]]++;
+        }
+    }
+    var list = [];
+    for(var j in map){
+        list.push([parseInt(j),map[j]]);
+    }
+    list = list.sort(function(a,b){ return b[1]-a[1] });
+    for(var l = 0,number = []; l < k; l++){
+        number.push(list[l][0]);
+    }
+    return number;
+};
