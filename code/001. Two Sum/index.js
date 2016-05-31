7884b717067e1005b4de0118bf50be9b7540c724
zj <mail@yangshaobo.cn>
2016-05-31 11:39:41 +0800

    ade Distribution(1)

--- code/001. Two Sum/index.js
+++ code/001. Two Sum/index.js
@@ -3,18 +3,36 @@
  * @param {number} target
  * @return {number[]}
  */
+//Distribution 80.54%,runtime 100ms
 var twoSum = function(nums, target) {
     var num = {};
     for(var i = 0, n = nums.length; i < n; i++){
         var end = target - nums[i];
         if(num[end] !== undefined){
             if(i > num[end]){
-                return [num[end],i]
+                return [num[end],i];
             }else{
-                return [i,num[end]]
+                return [i,num[end]];
             }
         }else{
             num[nums[i]] = i;
         }
+    }
+};
+//Distribution 51.73%,runtime 168ms
+var twoSum = function(nums, target) {
+    var num = [];
+    for(var i = 0, n = nums.length; i < n; i++){
+        var end = target - nums[i];
+        for(var j = 0,m = num.length; j < m; j++){
+            if(end === num[j]){
+                if(i > j){
+                    return [j,i];
+                }else{
+                    return [i,j];
+                }
+            }
+        }
+        num[i] = nums[i];
     }
 };
--- code/006. ZigZag Conversion/index.js
+++ code/006. ZigZag Conversion/index.js
@@ -3,7 +3,7 @@
  * @param {number} numRows
  * @return {string}
  */
- //runtime 95.33% 176ms
+//Distribution 95.33%,runtime 176ms
 var convert = function(s, numRows) {
     var res = "";
     if (numRows === 1) return s;
@@ -18,48 +18,48 @@ var convert = function(s, numRows) {
     }
     return res;
 };
-//runtime 12.15% 244ms
-// var convert = function(s, numRows) {
-//      var i, j;
-//      if(numRows === 1){
-//          return s;
-//      }
-//      var count = 0;
-//      var colDirect = true;
-//      var row = 0, col = 0;
-//      var map = [];
-//      for(i = 0; i < s.length; i++){
-//          map[i] = [];
-//      }
-//      for(i = 0; i < s.length; i++){
-//          if(colDirect){
-//              map[row][col] = s[i];
-//              row++;
-//              count++;
-//          }else{
-//              map[row][col] = s[i];
-//              row--;
-//              col++;           
-//              count++;
-//          }
-//          if(colDirect && count === numRows){
-//              count = 0;
-//              row -= 2;
-//              col++; 
-//              colDirect = false;
-//          }
-//          if(!colDirect && count === numRows - 2){
-//              count = 0;
-//              colDirect = true;
-//          }
-//      }
-//      var res = "";
-//      for(i = 0; i < map.length; i++){
-//          for(j = 0; j < map[i].length; j++){
-//              if(map[i][j]){
-//                  res += map[i][j];
-//              }
-//          }
-//      }
-//      return res;
-// };
+//Distribution 12.15%,runtime 244ms
+var convert = function(s, numRows) {
+     var i, j;
+     if(numRows === 1){
+         return s;
+     }
+     var count = 0;
+     var colDirect = true;
+     var row = 0, col = 0;
+     var map = [];
+     for(i = 0; i < s.length; i++){
+         map[i] = [];
+     }
+     for(i = 0; i < s.length; i++){
+         if(colDirect){
+             map[row][col] = s[i];
+             row++;
+             count++;
+         }else{
+             map[row][col] = s[i];
+             row--;
+             col++;           
+             count++;
+         }
+         if(colDirect && count === numRows){
+             count = 0;
+             row -= 2;
+             col++; 
+             colDirect = false;
+         }
+         if(!colDirect && count === numRows - 2){
+             count = 0;
+             colDirect = true;
+         }
+     }
+     var res = "";
+     for(i = 0; i < map.length; i++){
+         for(j = 0; j < map[i].length; j++){
+             if(map[i][j]){
+                 res += map[i][j];
+             }
+         }
+     }
+     return res;
+};
