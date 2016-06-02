6ba3b78f421346282491acce7f6a404b90c534a5
zj <mail@yangshaobo.cn>
2016-06-02 19:09:27 +0800

    144 add a new function

--- code/144. Binary Tree Preorder Traversal/index.js
+++ code/144. Binary Tree Preorder Traversal/index.js
@@ -9,6 +9,7 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+//递归
 //Distribution 82.14%,runtime 108ms
 var preorderTraversal = function(root) {
     var list = [];
@@ -20,5 +21,20 @@ var preorderTraversal = function(root) {
         }
     }
     preorder(root);
+    return list;
+};
+//迭代
+//Distribution 47.62%,runtime 119ms
+var preorderTraversal = function(root) {
+    var list = [];
+    var stack = [];
+    if(root === null) return list;
+    stack.unshift(root);
+    while(stack.length > 0){
+        tree = stack.shift();
+        list.push(tree.val);
+        if(tree.right !== null) stack.unshift(tree.right);
+        if(tree.left !== null) stack.unshift(tree.left);
+    }
     return list;
 };
