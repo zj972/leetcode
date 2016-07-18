cb06b39decd34a869bc37dab7bb64674868c6e98
zj <mail@yangshaobo.cn>
2016-07-18 14:29:02 +0800

    357. Count Numbers with Unique Digits

---
+++ code/357. Count Numbers with Unique Digits/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+//Distribution 88.89%,runtime 92ms
+var countNumbersWithUniqueDigits = function(n) {
+    if (n < 0) return 0;  
+    if (n === 0) return 1;  
+    if (n == 1) return 10;  
+    var counts = 10;  
+    var m = 9;  
+    for(var i=1, j=9; i<n && j>0; i++, j--){  
+        m *= j;  
+        counts += m;  
+    }  
+    return counts;  
+};
