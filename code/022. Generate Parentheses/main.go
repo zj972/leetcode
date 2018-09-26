d2f71fcd0c6cf14e165a007873f1a2b9bb7f0c94
zj972 <mail@yangshaobo.cn>
2018-09-26 20:30:38 +0800

    add 22. golang

---
+++ code/022. Generate Parentheses/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Runtime: 8 ms, faster than 100.00% of Go online submissions for Generate Parentheses.
+//type node struct {
+//	item string
+//	num  int
+//}
+//
+//func generateParenthesis(n int) []string {
+//	listNode := []*node{}
+//	list := []string{}
+//	listNode = add(&node{}, n)
+//	for j := 0; j < len(listNode); j++ {
+//		if listNode[j].num == 0 {
+//			list = append(list, listNode[j].item)
+//		} else {
+//			for k := 0; k < listNode[j].num; k++ {
+//				listNode[j].item = listNode[j].item + ")"
+//			}
+//			list = append(list, listNode[j].item)
+//		}
+//	}
+//	return list
+//}
+//
+//func add(l *node, n int) (o []*node) {
+//	if n == 0 {
+//		if l.num != 0 {
+//			for i := 0; i < l.num; i++ {
+//				l.item += ")"
+//			}
+//			l.num = 0
+//		}
+//		return []*node{l}
+//	}
+//	if l.num == 0 {
+//		o = add(&node{l.item + "(", 1}, n-1)
+//	} else {
+//		o = add(&node{l.item + "(", l.num + 1}, n-1)
+//		o = append(o, add(&node{l.item + ")", l.num - 1}, n)...)
+//	}
+//	return o
+//}
+
+//Runtime: 8 ms, faster than 100.00% of Go online submissions for Generate Parentheses.
+func generateParenthesis(n int) []string {
+	ret := make([]string, 0)
+	backtrackGP(&ret, "", 0, 0, n)
+	return ret
+}
+
+func backtrackGP(ret *[]string, cur string, open int, close int, max int) {
+	if len(cur) == max*2 {
+		*ret = append(*ret, cur)
+		return
+	}
+	if open < max {
+		backtrackGP(ret, cur+"(", open+1, close, max)
+	}
+	if close < open {
+		backtrackGP(ret, cur+")", open, close+1, max)
+	}
+}
+
+func main() {
+	fmt.Println(generateParenthesis(3))
+}
